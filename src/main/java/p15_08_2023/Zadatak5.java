5b6bbdb57c76d7facc2f3ba72f10e3514b53a07c
Jelena Lazic <jelena.lazic.novi.sad@gmail.com>
2023-08-15 23:08:28 +0200

    Add zadatak5

--- src/main/java/p15_08_2023/Zadatak5.java
+++ src/main/java/p15_08_2023/Zadatak5.java
@@ -3,10 +3,21 @@ package p15_08_2023;
 public class Zadatak5 {
     public static void main(String[] args) {
 
-        Character simbol = '*';
+
+        String zvezdica = "*";
+        String crtica = "|";
+        int count = 6;
+
+        System.out.println(zvezdica.repeat(count));
+        System.out.println(crtica.repeat(count));
 
 
-        System.out.println(simbol*6 );
+
+
+        
+
+
+
 
 
     }
