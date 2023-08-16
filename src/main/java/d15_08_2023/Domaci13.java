0854774e01e0dad6f3a61be550ebe20759f9d06d
Jelena Lazic <jelena.lazic.novi.sad@gmail.com>
2023-08-16 15:18:08 +0200

    Add Video quality functionality

---
+++ src/main/java/d15_08_2023/Domaci13.java
@@ -0,0 +1,28 @@
+package d15_08_2023;
+
+public class Domaci13 {
+    public static void main(String[] args) {
+
+        String nazivVidea = "Tea Tairovic - Bibi Habibi (Official Video / Album Balerina)";
+        String duzinaFile = "2:29";
+        int duzinaVidea = 0, brojFrame = 0;
+        String rezolucijaVidea = "1980x1080";
+        double rezolucija = 0, brzinaInterneta = 0;
+
+        duzinaVidea = 2*60+29;
+        brojFrame = 20;
+        rezolucija = 1920*1080;
+        brzinaInterneta = (rezolucija/(1024*1024))*brojFrame;
+
+
+
+
+        System.out.println("*********************************************");
+        System.out.println(nazivVidea);
+        System.out.println(duzinaFile+ "  <|||||");
+        System.out.println("Number of frames per second: "+brojFrame);
+        System.out.println("Resolution: "+rezolucijaVidea);
+        System.out.println("Minimal network speed: "+brzinaInterneta+"Mb/s");
+
+    }
+}
