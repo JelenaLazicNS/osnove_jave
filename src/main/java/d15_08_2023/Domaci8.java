00c78b83bf3cf95e1af31dff002e9a05d8396065
Jelena Lazic <jelena.lazic.novi.sad@gmail.com>
2023-08-16 13:33:54 +0200

    Add RGB

--- src/main/java/d15_08_2023/Domaci8.java
+++ src/main/java/d15_08_2023/Domaci8.java
@@ -3,5 +3,19 @@ package d15_08_2023;
 public class Domaci8 {
     public static void main(String[] args) {
 
+        int a = 0, b = 0, c = 0, e = 0, f = 0, g = 0;
+
+        a = 209;
+        b = 59;
+        c = 184;
+        e = 255 - a;
+        f = 255 - b;
+        g = 255 - c;
+
+        System.out.println("Originalna boja: RGB(" +a+ ", " +b+ ", " +c+ ")");
+        System.out.println("Kontrastna boja: RGB(" +e+ ", " +f+ ", " +g+ ")");
+
+
+
     }
 }
