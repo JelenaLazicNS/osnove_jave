b311bad26abcc2c73dc19e23bb661d8a1f266f71
Jelena Lazic <jelena.lazic.novi.sad@gmail.com>
2023-08-16 17:33:32 +0200

    Add resolution settings change

--- src/main/java/d15_08_2023/Domaci13.java
+++ src/main/java/d15_08_2023/Domaci13.java
@@ -6,22 +6,21 @@ public class Domaci13 {
         String nazivVidea = "Tea Tairovic - Bibi Habibi (Official Video / Album Balerina)";
         String duzinaFile = "2:29";
         int duzinaVidea = 0, brojFrame = 0;
-        String rezolucijaVidea = "1980x1080";
+        int a = 0, b =0;
         double rezolucija = 0, brzinaInterneta = 0;
 
         duzinaVidea = 2*60+29;
         brojFrame = 20;
-        rezolucija = 1920*1080;
+        a = 1920;
+        b = 1080;
+        rezolucija = a * b;
         brzinaInterneta = (rezolucija/(1024*1024))*brojFrame;
-
-
-
 
         System.out.println("*********************************************");
         System.out.println(nazivVidea);
         System.out.println(duzinaFile+ "  <|||||");
         System.out.println("Number of frames per second: "+brojFrame);
-        System.out.println("Resolution: "+rezolucijaVidea);
+        System.out.println("Resolution: "+a+"x"+b);
         System.out.println("Minimal network speed: "+brzinaInterneta+"Mb/s");
 
     }
