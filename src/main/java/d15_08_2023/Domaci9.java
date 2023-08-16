d00e84e44b7d4512ca980cded6c51dbe944e6608
Jelena Lazic <jelena.lazic.novi.sad@gmail.com>
2023-08-16 13:45:14 +0200

    Add file download estimation program

---
+++ src/main/java/d15_08_2023/Domaci9.java
@@ -0,0 +1,17 @@
+package d15_08_2023;
+
+public class Domaci9 {
+    public static void main(String[] args) {
+
+        String nazivFile = "profile-image.png";
+        String velOrigFile = "7";
+        String velDownlFile = "2";
+        String procenat = "28";
+
+        System.out.println(nazivFile+" "+velOrigFile+"Mb/"+velDownlFile+"Mb");
+        System.out.println("****************");
+        System.out.println("*****"+procenat+"%");
+        System.out.println("****************");
+
+    }
+}
