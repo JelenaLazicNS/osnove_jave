e9c6a0124dc91eeb7014f7a942949689bc470e89
Jelena Lazic <jelena.lazic.novi.sad@gmail.com>
2023-08-16 13:54:20 +0200

    Add file download estimated time

---
+++ src/main/java/d15_08_2023/Domaci10.java
@@ -0,0 +1,17 @@
+package d15_08_2023;
+
+public class Domaci10 {
+    public static void main(String[] args) {
+
+        String nazivFile = "profile-image.log";
+        String velFile = "25";
+        String brzinaSkidanja = "3";
+        String preostaloVreme = "8";
+
+        System.out.println("File: " + nazivFile);
+        System.out.println("Size: " + velFile+"Mb");
+        System.out.println("Download speed: "+ brzinaSkidanja+"Mb/s");
+        System.out.println("Estimated time: "+ preostaloVreme+"s");
+
+    }
+}
